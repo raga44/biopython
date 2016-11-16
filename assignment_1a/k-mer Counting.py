ebe26aca28ee0233e510f420907cae3afb6eef7f
raga44 <raga_4_4@yahoo.com>
2016-11-16 14:56:29 +0500

    Update k-mer Counting.py

--- assignment_1a/k-mer Counting.py
+++ assignment_1a/k-mer Counting.py
@@ -5,47 +5,31 @@ Created on Sun Oct 02 14:25:22 2016
 @author: Omer Javed
 """
 import random
-import matplotlib.pyplot as plt
-import numpy as np
-import itertools as it
 import time
+seqf = lambda L: ''.join(random.choice('ATGC') for _ in range(L))
 
-L= 32000
-k = 7
+def kmerCount(seq,k = 1):
+    D = {}
+    for i in range(len(seq)-k+1):
+        kmer = seq[i:i+k]
+        try:
+            D[kmer]+=1
+        except KeyError:
+            D[kmer] = 1
+    return D
 
-def genSeq(L):
-    dnaSeq= "".join(random.choice('GATC') for _ in range(L))
-    #print "genome sequence  of length ", L ,"",dnaSeq
-    return dnaSeq
-
-def kmer(k):
-    combination= it.product("TACG", repeat=k)
-    permutseq = list("".join(i) for i in combination)
-    #print "No of k-mers are = ", len(permutseq)
-    #print permutseq
-    return permutseq
-
-def matchedKmers(dnaSeq, permutseq):
-    count = 0
-    final=[]
-
-    for seq in permutseq:
-        count = dnaSeq.count(seq)
-        final.append([seq, count])
-    #print final
-    for i in final:
-        ''' print i ''' 
-t=[]
-a1=[]  
-
-dnaSeq = genSeq(L)
-for Kval in range(k):
-    start_time = time.clock()
-    permutseq=kmer(Kval)
-    matchedKmers(dnaSeq, permutseq)
-    total_time = time.clock() - start_time
-    a1.append(Kval)
-    t.append(total_time)
-        #print a1,t
-print a1,t       
-plt.plot(a1,t,color="red")
+Ls = [1000,10000,100000,1000000,10000000]
+Ks = [1,2,3,4,5]
+for L in Ls:
+    for k in Ks:
+        tmin = 1e10
+        for tries in range(5): #Repeat the experiment 5 times!
+            random.seed()
+            seq = seqf(L)
+            t0 = time.time()
+            D = kmerCount(seq,k)
+            dt = time.time()-t0
+            if dt<tmin: #stoe only the minimum time!
+                tmin = dt
+        
+        print "L = %d, k = %d : %f (s)" %(L,k,tmin)
